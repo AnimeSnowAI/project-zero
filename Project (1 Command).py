99200bc75f60a7faa55cc56503703b77d46f3840
AnimeSnowAI <ukraineanimeaicool@gmail.com>
2025-03-07 12:59:37 +0200

    Version 1.3
    Немного поработал над кодом, частично помог каджому и сделал задачу которая не была сделана, но все равно нам некогда расслаблятся, времени у нас немного и проблемы со светом, кто хочет доработать - доробатывайте, нам это будет кстати

--- Project (1 Command).py
+++ Project (1 Command).py
@@ -1,19 +1,6 @@
 from pygame import *
 from random import randint
 
-
-window = display.set_mode((800,600))
-display.set_caption("Project Zero")
-background = transform.scale(image.load("galaxy.jpg"),(800,600))
-
-
-
-
-
-
-
-
-
 class GameSprite(sprite.Spite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         super().__init__()
@@ -36,3 +23,31 @@ class Player(GameSprite):
             self.rect.x -= self.speed
         if keys[K_s] and self.rect.y < 450:
             self.rect.y += self.speed
+
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y += randint(1,5)
+        if self.rect.y > 500:
+            self.rect.x = randint(80,600)
+            self.rect.y = 0
+            
+window = display.set_mode((800,600))
+display.set_caption("Project Zero")
+background = transform.scale(image.load("background.png"),(800,600))
+font = font.SysFont('Arial', 80)
+pobeda = font.render('Ви виграли, дякуємо за гру!', True, (255, 255, 255))
+proigrish = font.render('Нажаль, ви не змогли пройти гру...', True, (180, 0, 0))
+
+PlayerX1 = Player("Player.png", 200,350,50,50,30)
+Enemy1 = Enemy("Enemy1.png", randint(80,600), randint(-100,0), 50, randint(1,6)
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish != True:
+        if sprite.spritecollide(PlayerX1, Enemy1, False):
+            finish = True
+            window.blit(proigrish, (200,200))
+    display.update()
+    
